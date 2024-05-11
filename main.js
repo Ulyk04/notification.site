57722b884d637c56d5dfc57cb1ccc6fa56de75f1
goldmeninalmaty <146926829+Ulyk04@users.noreply.github.com>
2024-05-12 00:03:25 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let wrapper = document.getElementById("wrapper");
+
+function wrapperOpen(){
+    wrapper.classList.add("wrapper-open");
+}
+function wrapperClose(){
+    wrapper.classList.remove("wrapper-open");
+}
